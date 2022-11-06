f0712ca7833888a1a6e5b4b83aac08c8312cbda3
nonyereeta <nonyeibeanu@gmail.com>
2022-11-06 19:14:36 +0100

    update to route

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ connectToDb()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', authRouter);
+// app.use('/', authRouter);
 app.use('articles/:email/create', passport.authenticate('jwt', { session: false }), articleRoute)
 app.use('articles/search/:arg', passport.authenticate('jwt', { session: false }), articleRoute)
 app.use('articles/sort/read_count', passport.authenticate('jwt', { session: false }), articleRoute)
